190326c7a85f28bf6aeda68905cd4c01c49ff747
SagarPrasad160 <sagarprasad916@gmail.com>
2023-07-21 12:55:07 +0530

    Add Typing Effect

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 // Add custom JavaScript here
+function typeText() {
+  const text = "I am Sagar Prasad.";
+  const typingTextEl = document.getElementById("typing-text");
+  const delay = 100;
+  typingEffect(text, typingTextEl, delay);
+}
+
+function typingEffect(text, typingTextEl, delay) {
+  for (let i = 0; i < text.length; i++) {
+    setTimeout(() => {
+      typingTextEl.textContent += text.charAt(i);
+    }, delay * i);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", typeText);
